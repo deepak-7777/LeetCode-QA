b0bf01f53c249a20b8e0d116a2b76d8f4bdb4ae5
Deepak Kumar <globalxwar76@gmail.com>
2026-08-30 11:56:05 +0530

    Time: 1 ms (96.61%) | Memory: 42.4 MB (73.93%) - LeetSync

--- 231-power-of-two/power-of-two.java
+++ 231-power-of-two/power-of-two.java
@@ -1,8 +1,8 @@
 class Solution {
     public boolean isPowerOfTwo(int n) {
-    if (n <= 0) {
+        if (n <= 0) {                    // base case 
         return false;
-    }
-    return (n & (n - 1)) == 0;
+        }
+    return (n & (n - 1)) == 0;   // & (And operator) dono number ke binary ko compare karta hai 
     }
 }

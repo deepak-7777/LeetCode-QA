48896a12933d60cab005c292f798709582a962b6
Deepak Kumar <globalxwar76@gmail.com>
2026-08-31 23:46:57 +0530

    Time: 764 ms (9.63%) | Memory: 79.8 MB (10.59%) - LeetSync

---
+++ 204-count-primes/count-primes.java
@@ -0,0 +1,31 @@
+class Solution {
+    public int countPrimes(int n) {
+        if (n == 0 && n == 1) {
+            return 0;
+        }
+
+        boolean[] isPrime = new boolean[n];
+
+        // Sabko initially prime maan lo
+        for (int i = 2; i < n; i++) {
+            isPrime[i] = true;
+        }
+
+        for (int p = 2; p * p < n; p++) {
+            if (isPrime[p]) {
+                for (int multiple = p * p; multiple < n; multiple += p) {
+                    isPrime[multiple] = false;
+                }
+            }
+        }
+
+        int count = 0;
+        for (int i = 2; i < n; i++) {
+            if (isPrime[i]) {
+                count++;
+            }
+        }
+
+        return count;
+    }
+}

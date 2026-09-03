33cb2d60c31afadca00f5647ad8fc178767dc42c
Deepak Kumar <globalxwar76@gmail.com>
2026-09-03 22:14:52 +0530

    Time: 12 ms (15.56%) | Memory: 52.7 MB (5.30%) - LeetSync

---
+++ 36-valid-sudoku/valid-sudoku.java
@@ -0,0 +1,35 @@
+import java.util.HashSet;
+
+class Solution {
+    public boolean isValidSudoku(char[][] board) {
+        HashSet<String> set = new HashSet<>();
+
+        for (int row = 0; row < 9; row++) {
+            for (int col = 0; col < 9; col++) {
+                char num = board[row][col];
+
+                // Empty cell skip karo
+                if (num == '.') {
+                    continue;
+                }
+                
+                String rowKey = num + " in row " + row;
+                String colKey = num + " in col " + col;
+                String boxKey = num + " in box " + (row / 3) + "-" + (col / 3);
+
+                // Agar pehle se present hai to duplicate mila
+                if (set.contains(rowKey) ||
+                    set.contains(colKey) ||
+                    set.contains(boxKey)) {
+
+                    return false;
+                }
+                // Store karo
+                set.add(rowKey);
+                set.add(colKey);
+                set.add(boxKey);
+            }
+        }
+        return true;
+    }
+}

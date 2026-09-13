df873f8df23905389068f7aed13d584650fa4797
Deepak Kumar <globalxwar76@gmail.com>
2026-09-14 02:06:17 +0530

    Time: 17 ms (98.75%) | Memory: 78.2 MB (34.17%) - LeetSync

--- 2728-sum-in-a-matrix/sum-in-a-matrix.java
+++ 2728-sum-in-a-matrix/sum-in-a-matrix.java
@@ -1,18 +1,18 @@
 class Solution {
     public int matrixSum(int[][] nums) {
-        int n = nums.length;
-        int m = nums[0].length;
+        int n = nums.length;      
+        int m = nums[0].length;   
 
-      for (int i = 0; i < n; i++) {
+      for (int i = 0; i < n; i++) {    // sort in ascending 
             Arrays.sort(nums[i]);
         }
 
-        int score = 0;
+        int score = 0;    // to store in final answer 
 
-        for (int j = 0; j < m; j++){
-            int max = 0;
-            for (int i = 0; i < n; i++) {
-                max = Math.max(max, nums[i][j]);
+        for (int j = 0; j < m; j++){    // for column 
+            int max = 0;   // current column ka maximum
+            for (int i = 0; i < n; i++) {     // for row 
+                max = Math.max(max, nums[i][j]);   // max find
             }
             score += max;
         }

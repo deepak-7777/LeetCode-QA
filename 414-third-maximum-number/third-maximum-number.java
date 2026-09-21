92ae11173f21d8499cadf011f7b9b59a67d524ab
Deepak Kumar <globalxwar76@gmail.com>
2026-09-22 00:24:50 +0530

    Time: 7 ms (28.88%) | Memory: 45 MB (36.92%) - LeetSync

--- 414-third-maximum-number/third-maximum-number.java
+++ 414-third-maximum-number/third-maximum-number.java
@@ -1,29 +1,18 @@
 class Solution {
     public int thirdMax(int[] nums) {
-        Long f = null;  // first 
-        Long s = null;  // second
-        Long t = null;  // third 
+        Arrays.sort(nums);
 
-        for (int num : nums) {
-            if ((f != null && num == f) ||       // Duplicate values ignore
-                (s != null && num == s) ||
-                (t != null && num == t)) {
-                continue;
-            }
-            if (f == null || num > f) {
-                t = s;
-                s = f;
-                f = (long) num;
-            } 
-            else if (s == null || num > s) {
-                t = s;
-                s = (long) num;
-            } 
-            else if (t == null || num > t) {
-                t = (long) num;
+        int count = 1;
+        int max = nums[nums.length - 1];
+
+        for (int i = nums.length - 2; i >= 0; i--) {
+            if (nums[i] != nums[i + 1]) {
+                count++;
+                if (count == 3) {
+                    return nums[i];
+                }
             }
         }
-        // Agar third maximum exist nahi karta
-        return (t == null) ? f.intValue() : t.intValue();
+        return max;
     }
 }

47f981178f072f4a8f978596c207e3c5a8c39059
Deepak Kumar <globalxwar76@gmail.com>
2026-09-22 23:22:01 +0530

    Time: 1 ms (98.11%) | Memory: 42.8 MB (25.60%) - LeetSync

--- 258-add-digits/add-digits.java
+++ 258-add-digits/add-digits.java
@@ -3,10 +3,10 @@ class Solution {
         while (num >= 10) {        // repeat until single digit
             int sum = 0;
             while (num > 0) {
-                sum = sum + (num % 10);
-                num = num / 10;
+                sum = sum + (num % 10);  // Last digit nikal kar sum me add
+                num = num / 10;          // Last digit hata do
             }
-            num = sum;
+            num = sum;      // Digits ke sum ko wapas num me store karo        
         }
         return num;
     }
